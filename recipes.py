c32118705e830a12a2e885cf99a16402232009ad
Гурылёв Илья <gurylev.crypto@gmail.com>
2026-06-04 00:32:07 +0300

    Реализован класс ShoppingList

--- recipes.py
+++ recipes.py
@@ -59,4 +59,43 @@ class Recipe:
         text = f"{self.title}\n"
         for ing in self.ingredients:
             text += str(ing) + "\n"
-        return text   
+        return text
+    
+
+class ShoppingList:
+
+    def __init__(self):
+        self._items = []
+
+    def add_recipe(self, recipe, portions):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+        scaled = recipe.scale(portions)
+        for ingredient in scaled.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+    def remove_recipe(self, title):
+        new_items = []
+        for ingredient, recipe_title in self._items:
+            if recipe_title != title:
+                new_items.append((ingredient, recipe_title))
+        self._items = new_items
+    
+    def get_list(self):
+        totals = {}
+        for ingredient, recipe_title in self._items:
+            key = (ingredient.name, ingredient.unit)
+            if key in totals:
+                totals[key] += ingredient.quantity
+            else:
+                totals[key] = ingredient.quantity
+        result = []
+        for (name, unit), quantity in totals.items():
+            result.append(Ingredient(name, quantity, unit))
+        result.sort(key=lambda ing: ing.name)
+        return result
+    
+    def __add__(self, other):
+        lst = ShoppingList()
+        lst._items = self._items + other._items
+        return lst
