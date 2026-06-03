7147d18c2e2b3156d8dc732ee6a518478167f5c3
Гурылёв Илья <gurylev.crypto@gmail.com>
2026-06-04 00:04:18 +0300

    Реализован класс Recipe

--- recipes.py
+++ recipes.py
@@ -23,3 +23,40 @@ class Ingredient:
     
     def __eq__(self, other):
         return self.name == other.name and self.unit == other.unit
+
+
+class Recipe:
+
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        if ingredients is None:
+            ingredients = []
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for e in self.ingredients:
+            if e == ingredient:
+                e.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным")
+        new_ingredients = []
+        for ing in self.ingredients:
+            new_ingredients.append(Ingredient(ing.name, ing.quantity * ratio, ing.unit))
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        text = f"{self.title}\n"
+        for ing in self.ingredients:
+            text += str(ing) + "\n"
+        return text   
